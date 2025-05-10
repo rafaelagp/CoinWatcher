7c434e02c8bbf34508b6e357ae1ab34cd2fe2229
Rafael Pereira <rafael.agp@globo.com>
2025-05-10 04:04:52 -0300

    Removed unused imports and added TODOs

--- app/src/main/java/net/rafgpereira/coinwatcher/ui/screen/main/MainScreen.kt
+++ app/src/main/java/net/rafgpereira/coinwatcher/ui/screen/main/MainScreen.kt
@@ -2,6 +2,7 @@ package net.rafgpereira.coinwatcher.ui.screen.main
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
@@ -35,9 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import net.rafgpereira.coinwatcher.R
@@ -163,12 +161,15 @@ fun CoinList(modifier: Modifier, data: List<Coin>) =
                     modifier = modifier
                         .height(dimensionResource(R.dimen.main_coin_item_height))
                         .fillMaxWidth()
-                        .background(
-                            when {
-                                isFirstRow -> remember { MidGrey.copy(alpha = 0.25f) }
-                                isThirdRow -> remember { MidGrey.copy(alpha = 0.5f) }
-                                else -> remember { MidGrey.copy(alpha = 0.9f) }
-                            }
+                        .background(// TODO review color
+                            if (isSystemInDarkTheme())
+                                MaterialTheme.colorScheme.background
+                            else
+                                when {
+                                    isFirstRow -> remember { MidGrey.copy(alpha = 0.25f) }
+                                    isThirdRow -> remember { MidGrey.copy(alpha = 0.5f) }
+                                    else -> remember { MidGrey.copy(alpha = 0.9f) }
+                                }
                         ),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
